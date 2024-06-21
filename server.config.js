b952bc68dac41e1dc852f6c695eefc5db06976e4
ashish8796 <ashishsaini.ak@gmail.com>
2024-06-22 00:19:24 +0530

    added NODE_ENV changes

--- server.config.js
+++ server.config.js
@@ -1,6 +1,15 @@
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? ".env.production"
+      : ".env.development",
+});
+
 export const port = process.env.PORT;
 export const dbUsername = process.env.DB_USERNAME;
 export const dbPassword = process.env.DB_PASSWORD;
 export const jwtSecret = process.env.JWT_SECRET;
-export const nodeEnv = process.env.NODE_ENV;
-export const hostname = process.env.HOSTNAME || "localhost";
+export const hostname = process.env.HOSTNAME;
